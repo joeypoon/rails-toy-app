50d312afed54d0442d731a99620eb159bdbcf0ac
Joey Poon <joey@joeypoon.com>
2015-04-19 00:11:18 -0500

    Complete exercise

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,8 @@ class Micropost < ActiveRecord::Base
 
   belongs_to :user
 
-  validates :content, length: {maximum: 140}
+  validates :content, length: {maximum: 140},
+                      presence: true
+  validates :user_id, presence: true
 
 end
